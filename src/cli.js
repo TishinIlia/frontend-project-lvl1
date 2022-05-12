888ea5fde742ec4a3f83c86505f4312d7fb9a853
nenaviju4tverg@gmail.com <HoroRedGuest1>
2022-05-11 23:38:01 -0700

    add eslint airbnb

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,11 @@
 import readlineSync from 'readline-sync';
 
-
 const greeting = () => {
-  console.log('Welcome to the Brain Games!')
-  console.log('May I have your name?')
+  console.log('Welcome to the Brain Games!');
+  console.log('May I have your name?');
   const name = readlineSync.question('Your answer: ');
 
-  console.log(`Hello, ${name}!`)
-}
+  console.log(`Hello, ${name}!`);
+};
 
-export default greeting
+export default greeting;
