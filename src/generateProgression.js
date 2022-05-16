f4a79f2f7d6801eed96ee8bf9603fc7bd5e0a09d
nenaviju4tverg@gmail.com <HoroRedGuest1>
2022-05-15 22:12:34 -0700

    update progression generation
    Took 17 minutes

--- src/generateProgression.js
+++ src/generateProgression.js
@@ -4,15 +4,14 @@ const generateProgression = () => {
   const start = generateNumber(1, 100);
   const step = generateNumber(1, 10);
   const length = 10;
-  const sequence = [start];
-  for (let i = 1; i < length; i += 1) {
-    sequence.push(start + step * i);
-  }
+  const sequence = Array.from({ length }, (_, i) => start + step * i);
   const chosenElement = generateNumber(0, 10);
   return {
     sequence: `${sequence.slice(0, chosenElement).join(' ')} ... ${sequence.slice(chosenElement + 1).join(' ')}`,
     answer: sequence[chosenElement],
   };
 };
+
+console.log(generateProgression());
 
 export default generateProgression;
